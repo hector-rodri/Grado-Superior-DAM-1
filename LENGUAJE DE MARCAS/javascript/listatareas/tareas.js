ebcdf9a6e6d1e015ad3f52fdef0d5f3410705728
hector-rodri <hrodriguez@nigul.cide.es>
2025-02-16 23:56:30 +0100

    masss js

---
+++ LENGUAJE DE MARCAS/javascript/listatareas/tareas.js
@@ -0,0 +1,18 @@
+function agregarTarea() {
+    let tareaInput = document.getElementById("nuevaTarea");
+    let lista = document.getElementById("listaTareas");
+
+    if (tareaInput.value.trim() === "") {
+        alert("Escribe una tarea antes de agregarla.");
+        return;
+    }
+
+    let nuevaTarea = document.createElement("li");
+    nuevaTarea.textContent = tareaInput.value;
+    nuevaTarea.onclick = function() {
+        lista.removeChild(nuevaTarea);
+    };
+
+    lista.appendChild(nuevaTarea);
+    tareaInput.value = "";
+}
