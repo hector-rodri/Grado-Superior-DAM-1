a4cb25721502ab275be91326f9051144730305e1
hector-rodri <hrodriguez@nigul.cide.es>
2025-02-21 11:13:22 +0100

    update musica

--- BASE DE DATOS/musica.sql
+++ BASE DE DATOS/musica.sql
@@ -104,27 +104,136 @@ INSERT INTO sales VALUES (11, 8, 11, DATE'2024-02-15', 3, 66);
 INSERT INTO sales VALUES (12, 9, 11, DATE'2024-08-25', 1, 22);
 
 --1: Mostra els noms i llinatges dels clients que hagin comprat algun album dels Beatles
+SELECT c.name, c.surname
+FROM customers c
+JOIN sales s ON c.id_customer = s.customer
+JOIN albums a ON s.album = a.id_album
+JOIN artists ar ON a.artist = ar.id_artist
+WHERE ar.name = 'The Beatles';
 
 --2: Mostra el nom i el preu dels àlbums amb un estoc inferior a 10
+SELECT a.title, a.price
+FROM albums a
+WHERE a.stock < 10;
 
 --3: Mostra el nom i la descripció dels gèneres que contenen la praula "rock".
+SELECT g.name, g.description
+FROM genres g
+WHERE UPPER(g.name) LIKE '%ROCK%';
 
 --4: Mostra el total de doblers gastat per cada client.
+SELECT c.name, c.surname, SUM(s.total) AS total_sales
+FROM customers c
+JOIN sales s ON c.id_customer = s.customer
+GROUP BY c.name, c.surname
+ORDER BY total_sales DESC;
 
 --5: Mostra el títol i l'any dels àlbums del gènere Pop.
+SELECT a.title, a.year
+FROM albums a
+JOIN genres g ON a.genre = g.id_genre
+WHERE g.name = 'Pop';
 
 --6: Mostra el nom i la biografia dels artistes que han venut més de 10 àlbums.
+SELECT ar.name, ar.biography
+FROM artists ar
+JOIN albums a ON ar.id_artist = a.artist
+JOIN sales s ON a.id_album = s.album
+GROUP BY ar.name, ar.biography
+HAVING SUM(s.quantity) > 10;
 
 --7: Mostra el preu promig dels àlbums per cada gènere, ordenats de menor a major.
+SELECT g.name, AVG(a.price) AS average_price
+FROM genres g
+JOIN albums a ON g.id_genre = a.genre
+GROUP BY g.name
+ORDER BY average_price ASC;
 
 --8: Mostra el nom i el cognom dels clients que no han fet cap compra.
+SELECT c.name, c.surname
+FROM customers c LEFT JOIN sales s ON c.id_customer = s.customer
+WHERE s.customer IS NULL;
 
 --9: Mostra el títol i el gènere dels àlbums que no s'han venut mai.
+SELECT a.title, g.name
+FROM albums a LEFT JOIN sales s ON a.id_album = s.album
+LEFT JOIN genres g ON a.genre = g.id_genre
+WHERE s.album IS NULL;
 
 --10: Mostra el nom i el país dels artistes que no tenen cap àlbum al catàleg de la botiga.
+SELECT ar.name, ar.country
+FROM artists ar LEFT JOIN albums a ON ar.id_artist = a.artist
+WHERE a.artist IS NULL;
 
 --11: Mostra el nom i la descripció dels gèneres que no tenen cap àlbum al catàleg de la botiga.
+SELECT g.name, g.description
+FROM genres g LEFT JOIN albums a ON g.id_genre = a.genre
+WHERE a.genre IS NULL;
 
 --12: Mostra el total d'ingressos obtinguts per les vendes de cada gènere d'àlbum. S'ha de veure el nom del gènere i els seus ingressos. Si un gènere no ha venut res, ha de sortir 0.
+SELECT gen.name, NVL(SUM(sal.total),0) FROM genres gen
+    LEFT JOIN albums alb ON alb.genre = gen.id_genre
+    LEFT JOIN sales sal ON sal.album = alb.id_album
+    GROUP BY gen.name;
 
 --13: Mostra el títol i l'artista (nom) de cada àlbum sense vendes, distingint si l'àlbum és del segle XX o del segle XXI.
+SELECT alb.title, art.name, CASE WHEN year < 2000 THEN 'XX' ELSE 'XXI' END FROM albums alb
+    JOIN artists art ON alb.artist = art.id_artist
+    LEFT JOIN sales sal ON sal.album = alb.id_album
+    WHERE sal.id_sale IS NULL;
+	
+--ACTIVITAT Cursos
+--1. Mostra el nom i el cognom dels estudiants que s'han matriculat al curs de 'Biologia General'
+SELECT students.name, students.surname FROM students 
+    JOIN enrollments ON students.id_student = enrollments.student
+	JOIN courses ON courses.id_course = enrollments.course
+	WHERE courses.name = 'Biologia General';
+
+--2. Mostra el nom i el departament dels professors que tenen un salari superior a 2500 euros.
+SELECT name, department FROM teachers
+	WHERE salary > 2500;
+
+--3. Mostra el nom i els crèdits dels cursos que imparteix el professor Albert Costa.
+SELECT courses.name, courses.credits FROM courses
+	JOIN teachers ON teachers.id_teacher = courses.teacher
+    WHERE teachers.name = 'Albert' and teachers.surname = 'Costa';
+
+--4. Mostra el valor i la data de les notes que ha obtingut l'estudiant Anna Garcia.
+SELECT grades.score, grades.at FROM grades
+    JOIN enrollments ON enrollments.id_enrollment = grades.enrollment
+	JOIN students ON enrollments.student = students.id_student
+	WHERE students.name = 'Anna' AND students.surname = 'Garcia';
+
+--5: Mostra el nom i el cognom dels estudiants que han obtingut una nota superior a 8 en algun curs. No hi pot haver repetits.
+SELECT students.name, students.surname FROM students
+	JOIN enrollments ON students.id_student = enrollments.student
+	JOIN grades On grades.enrollment = enrollments.id_enrollment
+    WHERE grades.score > 8;
+
+--6: Mostra el nom i el salari dels professors que imparteixen més de dos cursos.
+SELECT teachers.name, teachers.salary FROM teachers
+	JOIN courses ON courses.teacher = teachers.id_teacher
+	GROUP BY teachers.name, teachers.salary
+	HAVING COUNT(*) > 2;
+
+--7: Mostra el nom i la descripció dels cursos que tenen més de 3 estudiants matriculats.
+SELECT courses.name, courses.description FROM courses
+	JOIN enrollments ON enrollments.course = courses.id_course
+	JOIN students ON students.id_student = enrollments.student
+	GROUP BY courses.name, courses.description HAVING COUNT(*) > 3;
+
+--8: Mostra la mitjana de les notes de cada curs, ordenada de major a menor.
+SELECT courses.name, AVG(grades.score) as mitjana FROM courses
+	JOIN enrollments ON enrollments.course = courses.id_course
+	JOIN grades On grades.enrollment = enrollments.id_enrollment
+	GROUP BY courses.name ORDER BY mitjana desc;
+--9: Mostra el nom i el cognom dels estudiants que no han obtingut cap nota en cap curs. No hi pot haver repetits.
+SELECT DISTINCT students.id_student, students.name, students.surname FROM students
+	LEFT JOIN enrollments ON students.id_student = enrollments.student
+	LEFT JOIN grades On grades.enrollment = enrollments.id_enrollment
+    WHERE grades.id_grade IS NULL;
+
+--10: Mostra el nom i el departament dels professors que no imparteixen cap curs.
+SELECT teachers.name, teachers.department FROM teachers
+	LEFT JOIN courses ON courses.teacher = teachers.id_teacher
+	WHERE courses.id_course IS NULL;
