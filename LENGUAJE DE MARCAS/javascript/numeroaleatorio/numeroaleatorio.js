88a887abd9e0da6d397257286fef6e3cf2287b7c
hector-rodri <hrodriguez@nigul.cide.es>
2025-03-10 15:29:05 +0100

    update

--- LENGUAJE DE MARCAS/javascript/numeroaleatorio/numeroaleatorio.js
+++ LENGUAJE DE MARCAS/javascript/numeroaleatorio/numeroaleatorio.js
@@ -1,4 +1,18 @@
-function generarNumero() {
+var menuInicio = document.getElementById("botonesDificultad");
+function generarNumeroFacil() {
+    menuInicio.style.display = "none";
+    let numero = Math.floor(Math.random() * 10) + 1;
+    document.getElementById("numero").textContent = numero;
+}
+
+function generarNumeroMedio() {
+    menuInicio.style.display = "none";
+    let numero = Math.floor(Math.random() * 50) + 1;
+    document.getElementById("numero").textContent = numero;
+}
+
+function generarNumeroDificil() {
+    menuInicio.style.display = "none";
     let numero = Math.floor(Math.random() * 100) + 1;
     document.getElementById("numero").textContent = numero;
 }
