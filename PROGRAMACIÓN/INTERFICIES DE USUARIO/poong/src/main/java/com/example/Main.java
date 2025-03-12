d7db6aa8f8f514cb2f28368a282345c48a0c9d3e
hector-rodri <hrodriguez@nigul.cide.es>
2025-03-12 17:10:12 +0100

    update

---
+++ PROGRAMACIÓN/INTERFICIES DE USUARIO/poong/src/main/java/com/example/Main.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
