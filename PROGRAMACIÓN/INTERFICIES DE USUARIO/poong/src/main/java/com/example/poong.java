e98a9660e0e4bf65987ef551cc44dfd5cc469f40
hector-rodri <hrodriguez@nigul.cide.es>
2025-03-12 18:04:36 +0100

    update center

--- PROGRAMACIÓN/INTERFICIES DE USUARIO/poong/src/main/java/com/example/poong.java
+++ PROGRAMACIÓN/INTERFICIES DE USUARIO/poong/src/main/java/com/example/poong.java
@@ -31,13 +31,18 @@ public class poong extends JPanel implements ActionListener {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setColor(Color.RED);
-        g.fillOval(x, y, 20, 20);
+        g.setColor(new Color(240,51,255));
+        g.fillOval(x, y, 40, 40);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        if (x < 0 || x > getWidth() - 20) {
+            x = 400;
+            y = 300;
+        }
+        
         if (y < 0 || y > getHeight() - 20) {
             velocidadY = -velocidadY;
         }
