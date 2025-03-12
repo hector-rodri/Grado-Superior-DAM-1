7f8340ba5768db1a6818950c303e390dcc10cfe5
hector-rodri <hrodriguez@nigul.cide.es>
2025-03-12 17:22:04 +0100

    update

--- PROGRAMACIÓN/INTERFICIES DE USUARIO/poong/src/main/java/com/example/Main.java
+++ PROGRAMACIÓN/INTERFICIES DE USUARIO/poong/src/main/java/com/example/Main.java
@@ -6,6 +6,23 @@ import java.awt.event.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame ventana = new JFrame("Poong");
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ventana.setSize(800, 600);
+        ventana.setResizable(false);
+        ventana.setLocationRelativeTo(null);
+
+        
+        
+
+
+
+
+
+
+
+
+        ventana.setVisible(true);
+
     }
 }
