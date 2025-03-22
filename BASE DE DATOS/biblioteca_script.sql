a9c01af939c6bbc665dfc7d1c50931a964f18928
hector-rodri <hrodriguez@nigul.cide.es>
2025-03-22 14:32:24 +0100

    NUEVO SCRIPT CON BUCLE

--- BASE DE DATOS/biblioteca_script.sql
+++ BASE DE DATOS/biblioteca_script.sql
@@ -124,14 +124,78 @@ BEGIN
     FROM LLIBRE WHERE ID = id_llibre_comprovar;
     
     IF id_padre IS NULL THEN
-        DBMS_OUTPUT.PUT_LINE('El llibre ' || titol_llibre || ' no és seqüela de cap altre.');
+        DBMS_OUTPUT.PUT_LINE('El llibre ' || titol_llibre || ' no és seqüela de cap altre');
     ELSE
         SELECT L.TITOL, A.NOM INTO titol_padre, autor_padre
         FROM LLIBRE L
         JOIN AUTOR_LLIBRE AL ON L.ID = AL.ID_LLIBRE
         JOIN AUTOR A ON AL.ID_AUTOR = A.ID
         WHERE L.ID = id_padre;
-        DBMS_OUTPUT.PUT_LINE('El llibre ' || titol_llibre || ' és seqüela de ' || titol_padre || ' de ' || autor_padre || '.');
+        DBMS_OUTPUT.PUT_LINE('El llibre ' || titol_llibre || ' és seqüela de ' || titol_padre || ' de ' || autor_padre);
     END IF;
 END;
+
+/*2. Escriu un bloc anònim que, donat un ID de llibre i un nombre, incrementi el nombre
+d'exemplars del llibre en el nombre donat. Per exemple, si el llibre té 10 exemplars i
+el nombre donat és 7, al final de l'execucució del bloc ha de tenir 17 exemplars.
+Utilitza un FOR per incrementar els exemplars d'un amb un fins arribar al nombre desitjat.
+NO podeu incrementar-ho tot de cop.
+Imprimeix per pantalla un missatge a cada increment, a més dels valor inicial i final del
+nombre d'exemplars del llibre.
+
+OUTPUT d'exemple:
+El llibre amb ID 9 té 6 exemplars. S'ha sol·licitat afegir-li 4 exemplars.
+Afegit exemplar. Exemplars: 7
+Afegit exemplar. Exemplars: 8
+Afegit exemplar. Exemplars: 9
+Afegit exemplar. Exemplars: 10
+Exemplars finals: 10
+*/
+DECLARE
+    id_nuevo_llibre LLIBRE.ID%TYPE;
+    numero NUMBER := 10;
+    exemplars_nuevo_llibre LLIBRE.EXEMPLARS%TYPE;
+    
+BEGIN
+    SELECT LLIBRE.ID INTO id_nuevo_llibre FROM LLIBRE WHERE TITOL = 'Harry Potter i la cambra secreta';
+    SELECT EXEMPLARS INTO exemplars_nuevo_llibre FROM LLIBRE WHERE TITOL = 'Harry Potter i la cambra secreta';
+    
+     DBMS_OUTPUT.PUT_LINE('El llibre amb ID ' || id_nuevo_llibre || ' té ' || exemplars_nuevo_llibre || ' exemplars');
+     DBMS_OUTPUT.PUT_LINE('Hem de introduir ' || numero || ' exemplars');
+     
+     FOR i IN 1..numero LOOP
+     exemplars_nuevo_llibre := exemplars_nuevo_llibre + 1;
+     DBMS_OUTPUT.PUT_LINE('Afegit exemplar. Exemplars: ' || exemplars_nuevo_llibre);
+     END LOOP;
+     DBMS_OUTPUT.PUT_LINE('Exemplars finals: ' || exemplars_nuevo_llibre);
+END;
+
+/*3. Escriu un bloc anònim que, donat un ID de llibre i un nombre, decrementi el nombre
+d'exemplars del llibre segons el nombre donat, però sense poder passar mai de 0 exemplars.
+Per exemple, si el llibre té 2 exemplars i el nombre donat és 3, al final de l'execucució 
+del bloc ha de tenir 0 exemplars, no -1.
+Utilitza un FOR per decrementar els exemplars d'un amb un fins arribar al nombre desitjat
+o 0, segons el cas.
+NO podeu decrementar-ho tot de cop.
+Imprimeix per pantalla un missatge a cada decrement, a més dels valor inicial i final del
+nombre d'exemplars del llibre.
+Si no s'han pogut decrementar les vegades previstes per haver arribat a 0, indica-ho.
+
+OUTPUT d'exemple (No s'arriba a 0 exemplars):
+El llibre amb ID 9 té 6 exemplars. S'ha sol·licitat eliminar 4 exemplars.
+Eliminat exemplar. Exemplars: 5
+Eliminat exemplar. Exemplars: 4
+Eliminat exemplar. Exemplars: 3
+Eliminat exemplar. Exemplars: 2
+Exemplars finals: 2
+
+OUTPUT d'exemple (S'arriba a 0 exemplars):
+El llibre amb ID 7 té 3 exemplars. S'ha sol·licitat eliminar 5 exemplars.
+Eliminat exemplar. Exemplars: 2
+Eliminat exemplar. Exemplars: 1
+Eliminat exemplar. Exemplars: 0
+No queden més exemplars. 2 exemplars han quedat sense eliminar.
+Exemplars finals: 0
+*/
+
 
