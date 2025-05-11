f4b2ec693caed60951045326c5a813a970a1bb1a
hector-rodri <hrodriguez@nigul.cide.es>
2025-05-11 13:23:09 +0200

    metodo leerArchivo, ajustes generales y comentarios apartado interfaz

--- PROGRAMACIÓN/GESTIÓN ARCHIVOS/editordearchivos/src/main/java/com/example/editorDeArchivos.java
+++ PROGRAMACIÓN/GESTIÓN ARCHIVOS/editordearchivos/src/main/java/com/example/editorDeArchivos.java
@@ -2,13 +2,12 @@ package com.example;
 
 import javax.swing.*;//Importamos las librerias necesarias, para interfaza gráfica, diseño y eventos
 import java.awt.*;
-import java.awt.event.*;
 import java.io.*;
 
 public class editorDeArchivos extends JFrame {
 
-    private JTextField inputUsuario;
-    private JTextField escaparate;
+    private JTextField archivoUsuario;
+    private JTextArea escaparate;
     private JCheckBox implementar;
 
     public editorDeArchivos(){
@@ -22,54 +21,57 @@ public class editorDeArchivos extends JFrame {
         JFrame ventana = new JFrame("EDITOR DE ARCHIVOS DE TEXTO");//Creamos la ventana
         ventana.setSize(600, 500);//Tamaño de la ventana
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Indicamos que al cerrar la ventana, se cierre el programa
-        ventana.setLocationRelativeTo(null);
-        ventana.setResizable(false);
+        ventana.setLocationRelativeTo(null);//Colocamos la ventana en el centro de la pantalla
+        ventana.setResizable(false);//No se puede cambiar el tamaño de la ventana
         //PANEL PRINCIPAL
         JPanel panelPrincipal = new JPanel();//Creamos un panel principal
-        panelPrincipal.setLayout(new BorderLayout());//Le damos un diseño GridLayout de 4 filas y 1 columna
-        ventana.add(panelPrincipal);//Agregamos el panel a la ventana
+        panelPrincipal.setLayout(new BorderLayout());//Le damos un diseño BorderLayout
+        ventana.add(panelPrincipal);
         //PANEL DE TEXTO
-        JPanel panelTexto = new JPanel();//Creamos un panel para el texto
+        JPanel panelTexto = new JPanel();//Creamos un panel para guardar el JLabel y el JTextField
         panelTexto.setLayout(new GridLayout(1, 2));//Le damos un diseño GridLayout de 1 fila y 2 columnas
-        panelPrincipal.add(panelTexto,BorderLayout.NORTH);//Agregamos el panel de texto al panel principal
-        JLabel texto1 = new JLabel("NOMBRE DEL ARCHIVO");// Creamos un texto
-        panelTexto.add(texto1);//Agregamos el texto al panel de texto
-        inputUsuario = new JTextField();// Creamos zonas de texto
-        panelTexto.add(inputUsuario);
+        panelPrincipal.add(panelTexto,BorderLayout.NORTH);//Agregamos el panel de texto al panel principal con direección norte
+        JLabel texto1 = new JLabel("NOMBRE DEL ARCHIVO");//Creamos un JLabel
+        panelTexto.add(texto1);
+        archivoUsuario = new JTextField();//Creamos un JTextField donde el usuario escribirá el nombre del archivo
+        panelTexto.add(archivoUsuario);
         //TEXTO 
-        escaparate = new JTextField();
+        escaparate = new JTextArea();//Creamos un JTextArea donde se leera y escribirá el texto
         panelPrincipal.add(escaparate);
         //PANEL DE BOTONES Y CHECKBOX
-        JPanel panelBotones = new JPanel();
-        panelPrincipal.add(panelBotones,BorderLayout.SOUTH);
-        JButton leer = new JButton("LEER");
-        leer.addActionListener(e -> leerArchivo());
-        JButton escribir = new JButton("ESCRIBIR");
-        escribir.addActionListener(e -> {
+        JPanel panelBotones = new JPanel();//Creamos un panel para los botones y el checkbox
+        panelPrincipal.add(panelBotones,BorderLayout.SOUTH);//Agregamos el panel de botones al panel principal con dirección sur
+        JButton leer = new JButton("LEER");//Creamos un botón para leer el archivo
+        leer.addActionListener(e ->{//Agregamos un evento al botón para leer el archivo
+            try {
+                leerArchivo();
+            } catch (IOException io) {
+                io.printStackTrace();
+            }
+        });
+        JButton escribir = new JButton("ESCRIBIR");//Creamos un botón para escribir el archivo
+        escribir.addActionListener(e -> {//Agregamos un evento al botón para escribir el archivo
             try {
                 escribirArchivo();
             } catch (IOException io) {
                 io.printStackTrace();
             }
         });
-        
-        implementar = new JCheckBox("AÑADIR");
+        implementar = new JCheckBox("AÑADIR");//Creamos un checkbox para añadir texto al archivo
         panelBotones.add(leer);
         panelBotones.add(escribir);
         panelBotones.add(implementar);
         ventana.setVisible(true);
-
     }
 
     public static void main(String[] args) {
-        new editorDeArchivos();//Creamos una nueva ventana
-        
+        new editorDeArchivos();//Creamos una nueva ventana de editor de archivos
     }
 
     private void escribirArchivo() throws IOException{
         FileOutputStream fileEscribir = null;
         char caracter;
-        String nombreArchivo = inputUsuario.getText();
+        String nombreArchivo = archivoUsuario.getText();
         String texto = escaparate.getText();
         Boolean masTexto = false;
 
@@ -78,7 +80,7 @@ public class editorDeArchivos extends JFrame {
         }
 
         if (nombreArchivo.isEmpty() || texto.isEmpty()) {
-            JOptionPane.showMessageDialog(this, "Por favor ingrese un nombre de archivo y contenido", "Error", 2);
+            JOptionPane.showMessageDialog(this, "Por favor ingrese un nombre de archivo y contenido", "Error", JOptionPane.ERROR_MESSAGE);
             return;
         }
 
@@ -93,10 +95,27 @@ public class editorDeArchivos extends JFrame {
         }finally{
             fileEscribir.close();
         }
-    
     }
 
-    private void leerArchivo() {
-        JOptionPane.showMessageDialog(rootPane, "EN DESARROLLO...", "Advertencia", 2);  
-    }  
+    private void leerArchivo() throws IOException{
+        FileInputStream file = null;
+        String nombreArchivo = archivoUsuario.getText();
+        String texto = "";
+        char caracter;
+
+        try {
+            file = new FileInputStream(nombreArchivo);
+            int tamano = file.available();
+            for (int i = 0; i < tamano; i++) {
+                caracter = (char) file.read();
+                texto += caracter;
+            }
+            escaparate.setText(texto);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            file.close();
+        }
+    }
 }
